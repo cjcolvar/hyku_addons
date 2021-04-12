8b5b410de9b8d5e7dc8bb811d8a880080d9340a1
Chris Colvard <chris.colvard@gmail.com>
2021-04-12 06:38:22 -0400

    Simple failing test showing mass delegate isn't working as expected

--- spec/presenters/hyrax/pacific_book_presenter_spec.rb
+++ spec/presenters/hyrax/pacific_book_presenter_spec.rb
@@ -4,7 +4,17 @@
 require 'rails_helper'
 
 RSpec.describe Hyrax::PacificBookPresenter do
-  it "has tests" do
-    skip "Add your tests here"
+  let(:presenter) { described_class.new(solrdoc, nil, nil) }
+  let(:work) { Hyrax::PacificBook.new }
+  let(:solrdoc) { SolrDocument.new(work.to_solr, nil) }
+
+  let(:additional_properties) do
+    [:is_included_in]
+  end
+
+  describe 'accessors' do
+    it 'defines accessors' do
+      additional_properties.each { |property| expect(presenter).to respond_to(property) }
+    end
   end
 end
